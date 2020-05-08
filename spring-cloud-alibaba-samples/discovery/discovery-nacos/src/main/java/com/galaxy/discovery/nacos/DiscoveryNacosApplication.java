b373b61bd9f448ed51aa60eb5dde056356eb661b
galaxy <469753862@qq.com>
2020-05-08 21:27:41 +0800

    discovery   nacos

---
+++ spring-cloud-alibaba-samples/discovery/discovery-nacos/src/main/java/com/galaxy/discovery/nacos/DiscoveryNacosApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.discovery.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryNacosApplication.class, args);
+    }
+
+}
